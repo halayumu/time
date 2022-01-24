4a1b36c793df0c4a995e11fd4c3bf3fedd90b731
halayumu <ookamiayumuwww@gmail.com>
2022-01-24 15:56:30 +0900

    リアルタイム時計処理作成

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
 new Vue({
     el: "#app",
+
     //----[htmlのテキスト情報]----//
     data: {
         heading: "現在の天気予報",
+        time: ''
     },
 
+    //----[現在の日付・時刻を取得処理]----//
     methods: {
-        updateTime: function() { //現在の日付・時刻を取得 
-            let time = new Date()
-            return time;
+        updateTime: function() {
+            let current = new Date()
+            this.time = current.getHours() + ':' + current.getMinutes() + ':' + current.getSeconds();
         }
+    },
+
+    //----[秒数の追加処理]----//
+    mounted: function() {
+        return setInterval(this.updateTime, 1000);
     }
 })
