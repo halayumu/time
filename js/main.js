a9a69fcb760591455ed82a3b7475f246108be56c
halayumu <ookamiayumuwww@gmail.com>
2022-01-29 23:22:25 +0900

    関数作成調整中

--- js/main.js
+++ js/main.js
@@ -4,19 +4,26 @@ new Vue({
     //----[htmlのテキスト情報]----//
     data: {
         heading: "現在時間",
-        time: ''
+        time: '',
+        // url: "https: //map.yahooapis.jp/weather/V1/place?coordinates=139.732293,35.663613&appid=",
+        // appId: "dj00aiZpPVh2N3kzZUxwelF5biZzPWNvbnN1bWVyc2VjcmV0Jng9ZDM-"
     },
 
-    //----[現在の日付・時刻を取得処理]----//
     methods: {
+        //----[現在の日付・時刻を取得処理]----//
         updateTime: function() {
             let current = new Date()
             this.time = current.getHours() + ':' + current.getMinutes() + ':' + current.getSeconds();
+        },
+
+        //----[天気情報]----//
+        weather: function() {
+
         }
     },
-
     //----[秒数の追加処理]----//
     mounted: function() {
         return setInterval(this.updateTime, 1000);
     }
+
 })
